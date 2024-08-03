9a662d7b86e6e904010b34907eadfd60cae954a6
Balvantkushwaha <kushwahabalvant625@gmail.com>
2024-08-03 11:12:02 +0530

    add content

---
+++ RecordingClass/bigdecimal.java
@@ -0,0 +1,15 @@
+package RecordingClass;
+
+import java.math.BigDecimal;
+
+class bigdecimal {
+   public static void main(String args[]){
+    BigDecimal a = new BigDecimal(0.06);
+    BigDecimal b = new BigDecimal(0.04);
+    BigDecimal c = a.subtract(b);
+    System.out.println(c);
+    float d = 1.4f;
+    System.out.println(d);
+   }
+    
+} 
