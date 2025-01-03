c4ec5a51fe09ae2f96d10417941df3a9ae1f98cf
Balvantkushwaha <kushwahabalvant625@gmial.com>
2025-01-03 08:52:18 +0530

    printNumber 1 to 10

---
+++ AssignmentCode/printNumber.java
@@ -0,0 +1,9 @@
+package PwskillJava.AssignmentCode;
+public class printNumber {
+    public static void main(String args[]){
+        int n=10 ;
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
