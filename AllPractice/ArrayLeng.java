88dd03089f21a05bf7dee5ff6913e8d045fd1d6d
Balvantkushwaha <kushwahabalvant625@gmial.com>
2025-01-30 23:16:12 +0530

    assign ment quesiton

---
+++ AllPractice/ArrayLeng.java
@@ -0,0 +1,9 @@
+package AllPractice;
+
+public class ArrayLeng {
+
+    public static void main(String[] args) {
+        String str = "Welcome to PW skill";
+        System.err.println("lenth of string of using length()mehtod is "+str.length());
+    }
+}
---
+++ AllPractice/Arraylength.java
@@ -0,0 +1,9 @@
+ package AllPractice;
+
+class Arraylength {
+     public static void main(String[] args) {
+         int array[] ={2,4,5,4,6,5,7};
+         System.out.println("lenth of array "+array.length);
+     }
+    
+}
