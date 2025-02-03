66172fe65c971ebe8fd9159222b2edf666bbd59a
Balvantkushwaha <kushwahabalvant625@gmial.com>
2025-02-03 18:06:10 +0530

    assignment question solved

--- AllPractice/ArrayLeng.java
+++ AllPractice/ArrayLeng.java
@@ -4,6 +4,25 @@ public class ArrayLeng {
 
     public static void main(String[] args) {
         String str = "Welcome to PW skill";
+     
         System.err.println("lenth of string of using length()mehtod is "+str.length());
+        int arr[] = new int [5];
+
+        System.out.print(arr);
+        System.out.println("=============");
+        String s=new String(); 
+        System.out.println(s);
+        System.out.println("======");
+        StringBuilder sb1 = new StringBuilder("123");
+
+          String s1 = "123";
+
+       // insert code here
+           //sb1.append(“abc”); s1.append(“abc”);
+         System.out.println(sb1 + " " + s1);
+
+
+          
     }
+
 }
